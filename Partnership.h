dcd5fd6b12f142ef2f8b28066c7ecc6b5d20e9a5
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-10-16 18:31:42 +1030

    cheer

--- Partnership.h
+++ Partnership.h
@@ -1,30 +1,33 @@
 #ifndef PARTNERSHIP_H
 #define PARTNERSHIP_H
-#include "User.h"
-#include "Investor.h"
-#include <iostream>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
-class Partnership:public Investor
-{
-    private:
-        std::string *First_Name;
-        std::string *Last_Name;
-        int Number_Of_Partners;
-    public:
-        Partnership(); // Default Constructor
-        Partnership(int ID); // Constructor
-        void Get_Partnership_Details(); // Sets partners First Name and Last Name Per Accounts.csv
-        void Print_Partnership_Portfolio(); // Creates CSV of portfolio with personalised details for Individual
-        void Partnersip_Display_Results_Test(); // Test for Displaying Results
-        void Set_Partners_Count(); // Sets Partner count
-        void Set_Partners_Names(); // Sets Partners First Names
-        void Print(); // Overloading inherited function
-        ~Partnership(); // Destructor
+#include "Investor.h"
+#include "User.h"
 
+class Partnership : public Investor {
+ private:
+  std::string *First_Name;
+  std::string *Last_Name;
+  int Number_Of_Partners;
+
+ public:
+  Partnership();                   // Default Constructor
+  Partnership(int ID);             // Constructor
+  void Get_Partnership_Details();  // Sets partners First Name and Last Name Per
+                                   // Accounts.csv
+  void Print_Partnership_Portfolio();  // Creates CSV of portfolio with
+                                       // personalised details for Individual
+  void Partnersip_Display_Results_Test();  // Test for Displaying Results
+  void Set_Partners_Count();               // Sets Partner count
+  void Set_Partners_Names();               // Sets Partners First Names
+  void Print();                            // Overloading inherited function
+  ~Partnership();                          // Destructor
 };
 
 #endif
--- User.h
+++ User.h
@@ -1,21 +1,22 @@
 #ifndef USER_H
 #define USER_H
-#include <iostream>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
-class User
-{
-    protected:
-        int Logged_In;
-        int Account_Number; // Account Number
-        std::string Account_Type; // String indicating Account_Type
-        int Current_Year;
-    public:
-        std::string Get_Account_Type(); //Function Returning Account Type
-        void Set_Account_Type(); //Function Setting Account Type
+class User {
+ protected:
+  int Logged_In;
+  int Account_Number;        // Account Number
+  std::string Account_Type;  // String indicating Account_Type
+  int Current_Year;
+
+ public:
+  std::string Get_Account_Type();  // Function Returning Account Type
+  void Set_Account_Type();         // Function Setting Account Type
 };
 
 #endif
